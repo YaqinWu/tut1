80a49b788a661fcf4c9fb42a6bcc05dd72d2adbe
Yaqin Wu <wuyaqin47@gmail.com>
2022-07-31 10:28:05 +0800

    Added feature 3: get user input from console

--- leapYear.java
+++ leapYear.java
@@ -1,12 +1,12 @@
 // Java program to check if the input year is a leap year or not
-
+import java.util.Scanner; 
  
 public class leapYear{
  
         public static void main(String[] args) {
-               
+                Scanner s = new Scanner(System.in);
                 //year we want to check
-                int year = 2004;
+                int year = s.nextInt();
                
                 //if year is divisible by 4, it is a leap year
                
@@ -14,6 +14,7 @@ public class leapYear{
                         System.out.println("Year " + year + " is a leap year");
                 else
                         System.out.println("Year " + year + " is not a leap year");
+				s.close();
         }
 }
  
